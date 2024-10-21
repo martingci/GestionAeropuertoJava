e44ca88f3d845c9838c763b7db81ba03d1ec35b2
martingci <m.carrasco29@ufromail.cl>
2024-10-20 22:13:26 -0300

    Se crean automáticamente las clases por medio de Visual Paradigm

---
+++ src/main/java/org/example/Aeropuerto.java
@@ -0,0 +1,40 @@
+package org.example;
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Aeropuerto {
+	private String ubicacion;
+	public ArrayList<Avion> aviones = new ArrayList<Avion>();
+
+	public String getUbicacion() {
+		return this.ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public void crearVuelo(String origen, String destino, Date horaSalida, Date horaLlegada) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void registrarAvion(String nombre) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void buscarVuelosPorHora(Date hora) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void buscarUsuariosdeVuelo(Date hora, String origen, String destino) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Aeropuerto(String ubicacion, ArrayList<Avion> aviones) {
+		throw new UnsupportedOperationException();
+	}
+
+	public ArrayList<Avion> getAviones() {
+		throw new UnsupportedOperationException();
+	}
+}
